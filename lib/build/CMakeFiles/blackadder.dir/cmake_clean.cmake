9cf337f62ce8dc5b09f1f597a3c32a6bcb5151bf
parisis <parisis@debian>
2015-03-17 19:50:23 +0000

    deleted build folder

--- lib/build/CMakeFiles/blackadder.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "libblackadder.pdb"
-  "libblackadder.a"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/blackadder.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
